8e36cc0e2f4d4e5ed7e06e9c08f87dff478a7124
gelcipereira <167432708+gelcipereira@users.noreply.github.com>
2025-02-28 08:57:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
